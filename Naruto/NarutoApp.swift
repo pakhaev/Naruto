5ff0184aa00477a41390c1d09c9d736662f192d8
Пахаев Хусейн <pakhaev01@gmail.com>
2023-07-31 11:06:22 +0300

    Initial Commit

---
+++ Naruto/NarutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoApp.swift
+//  Naruto
+//
+//  Created by Khusain on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NarutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
